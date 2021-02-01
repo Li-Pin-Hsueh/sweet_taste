5cccef1ab24e19033e0962b8b92d98d1fb27d5a9
Pin Hsueh <pinxue.life@gmail.com>
2021-02-01 20:39:57 +0800

    product component test completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
+import { ProductsComponent } from './products/products.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router'; // CLI imports router
 import { HomeComponent } from "./home/home.component";
 
 const routes: Routes = [
-  {path:'', component:HomeComponent}
-  
+  { path:'', component:HomeComponent},
+  { path:'products', component:ProductsComponent},
+
 ]; // sets up routes constant where you define your routes
 
 // configures NgModule imports and exports
