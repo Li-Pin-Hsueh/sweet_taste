2c851f11fec425b9f9d7e7d8822dcd8560331b59
Vincent <pinxue.career@gmail.com>
2021-02-01 16:46:04 +0800

    build home comp template

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
+import { Product } from "../product";
 
 @Component({
   selector: 'app-home',
@@ -8,10 +9,37 @@ import { ProductService } from '../product.service';
 })
 export class HomeComponent implements OnInit {
 
+  selectedType = 'daily' ;
+  displayProducts : Product[] = [];
+
   constructor(public productService : ProductService) { }
 
   ngOnInit(): void {
     this.productService.ngOnInit();
+  }
+
+  getTopProducts( type: string ) : void {
+    const allProducts = this.productService.products ;
+    let count = 0 ;
+
+    this.selectedType = type ;
+    this.displayProducts = [];
+
+    for( let index=0; index < allProducts.length ; index ++){
+      if( allProducts[index].tags.includes(type) ){
+        this.displayProducts.push(allProducts[index]) ;
+        count ++ ;
+        if(count == 3) break ;
+      }
+    }
+
+
+    //console.log(this.topProducts) ;
+  }
+
+  addToCart(product) {
+    //TODO
+    //this.cartService.addToCart(product) ;
   }
 
 }
